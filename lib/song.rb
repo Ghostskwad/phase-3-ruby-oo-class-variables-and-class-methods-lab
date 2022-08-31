2dc0e4583bf4efa381c81b3df04628ce5769682b
Quade Houston <quadehouston@gmail.com>
2022-08-31 17:02:01 -0400

    commit tree 020c23ce66f5d5662bf904fe3542e755c2bd6e1c

---
+++ lib/song.rb
@@ -0,0 +1,44 @@
+require 'pry'
+class Song
+    attr_accessor :name, :artist, :genre
+
+    @@count = 0
+    @@artists = []
+    @@genres = []
+    @@artist_count = {}
+    @@genre_count = {}
+
+    def initialize name, artist, genre
+        @name = name
+        @artist = artist
+        @genre = genre
+        @@count += 1
+        @@artists << self.artist
+        @@genres << self.genre
+    end
+
+    def self.count
+        @@count
+    end
+
+    def self.artists
+        @@artists.uniq
+    end
+
+    def self.genres
+        @@genres.uniq
+    end
+
+    def self.artist_count
+        @@artists.tally 
+    end
+
+    def self.genre_count
+        @@genres.tally
+    end
+
+
+
+end
+
+
